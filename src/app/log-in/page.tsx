020ecd1702eea1ca5ac1e95ce4cf31e8f6376fac
RadojkoRadulovic <radulovicradojko@gmail.com>
2025-06-20 11:57:52 -0400

    added Login page

--- src/app/log-in/page.tsx
+++ src/app/log-in/page.tsx
@@ -1,3 +1,19 @@
+
 export default function LogIn() {
-  return <h2>LogIn Page</h2>;
+  return (
+    <main className="mainContainer flex-center flex-grow">
+      <div className="w-full sm:w-xl max-w-xxl bg-white shadow-lg rounded-xl p-8">
+        <h1 className="text-2xl font-bold text-center text-gray-800 mb-6">
+          Prijavi se
+        </h1>
+       
+        {/* <p className="mt-4 text-sm text-center text-gray-600">
+          Već imaš nalog?{" "}
+          <Link href="/login" className="text-blue-600 hover:underline">
+            Prijavi se
+          </Link>
+        </p> */}
+      </div>
+    </main>
+  );
 }
