15425974708484157b427d47bfe17de494822734
RadojkoRadulovic <radulovicradojko@gmail.com>
2025-06-23 20:08:48 -0400

    ....

--- src/lib/authAction.ts
+++ src/lib/authAction.ts
@@ -9,7 +9,7 @@ import { logToFile } from './logger';
 
 
 // SIGNUP FORM ACTION
-export async function signupFormAction(formData: FormData){
+export async function signupFormAction(formData: FormData) {
   try {
     logToFile("signupFormAction pokrenut");
 
@@ -18,35 +18,38 @@ export async function signupFormAction(formData: FormData){
     const userPassword = formData.get('password');
 
     logToFile(`Primljeni podaci: company_name=${companyNameRaw}, email=${userEmail}`);
-    
+
 
     if (
-        typeof companyNameRaw !== 'string' ||
-        typeof userEmail !== 'string' ||
-        typeof userPassword !== 'string'
-      ) {
-        throw new Error('Sva polja su obavezna i moraju biti stringovi');
-      }
+      typeof companyNameRaw !== 'string' ||
+      typeof userEmail !== 'string' ||
+      typeof userPassword !== 'string'
+    ) {
+      throw new Error('Sva polja su obavezna i moraju biti stringovi');
+    }
 
-      const hashedPassword = await hashPassword(userPassword)
+    const hashedPassword = await hashPassword(userPassword)
 
     const user = {
-        company_name:companyNameRaw,
-        company_slug: slugify(companyNameRaw, {lower:true, strict: true }),
-        user_email: userEmail,
-        user_password: hashedPassword
+      company_name: companyNameRaw,
+      company_slug: slugify(companyNameRaw, { lower: true, strict: true }),
+      user_email: userEmail,
+      user_password: hashedPassword
     }
 
     logToFile(`Korisnik uspešno upisan: ${companyNameRaw}`);
 
     await saveUser(user);
-    redirect(`/${user.company_slug}`)
+    console.log("pre redirect");
+    
+     redirect(`/${user.company_slug}`)
+
   } catch (error: unknown) {
     if (
       error instanceof Error &&
       error.message === 'NEXT_REDIRECT'
     ) {
-      return;
+     throw error
     }
 
     if (error instanceof Error) {
@@ -54,35 +57,35 @@ export async function signupFormAction(formData: FormData){
     } else {
       console.error("Nepoznata greška u signupFormAction.");
     }
-  
+
     throw error;
   }
 
+}
+
+export async function loginFormAction(formData: FormData) {
+  const email = formData.get("email");
+  const password = formData.get("password");
+
+  if (typeof email !== "string" || typeof password !== "string") {
+    throw new Error("Email i lozinka su obavezni")
   }
 
-  export async function loginFormAction(formData: FormData) {
-    const email = formData.get("email");
-    const password = formData.get("password");
+  const user = await getUserByEmail(email);
 
-    if(typeof email !== "string" || typeof password !== "string") {
-      throw new Error("Email i lozinka su obavezni")
-    }
-
-    const user = await getUserByEmail(email);
-
-    if(!user) {
-      throw new Error('Ne postoji korisnik sa tim emailom');
-    }
-
-    const isValid = await comparePassword(password, user.user_password);
-
-    if (!isValid) {
-      throw new Error('Pogrešna lozinka');
-    }
-
-    redirect(`/${user.company_slug}/dashboard`)
-
+  if (!user) {
+    throw new Error('Ne postoji korisnik sa tim emailom');
   }
+
+  const isValid = await comparePassword(password, user.user_password);
+
+  if (!isValid) {
+    throw new Error('Pogrešna lozinka');
+  }
+
+  redirect(`/${user.company_slug}/dashboard`)
+
+}
 
 //   SIGNIN FORM ACTION
 // export async function loginFormAction(formData: FormData) {
